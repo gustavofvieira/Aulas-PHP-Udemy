cef910b1661fbd7a302fdc9da6b4f715ec03767c
Gustavo Vieira <gustavofvieira@live.com>
2019-08-29 22:24:31 -0300

    Subindo PDO com DAO

--- 03-Banco de Dados/03-PDO/02-Data-Access-Object-e-PDO/config.php
+++
@@ -1,8 +0,0 @@
-<?php
- 
- spl_autoload_register(function($class_name){
-    $filename = "classes".DIRECTORY_SEPARATOR.$class_name.".php";
-    if(file_exists(($filename))){
-        require_once($filename);
-    }
- });
--- 03-Banco de Dados/03-PDO/02-Data-Access-Object-e-PDO/procedure.sql
+++
@@ -1,8 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_usuarios_insert`(
-pdeslogin varchar(64),
-pdessenha varchar(256)
-)
-BEGIN
-INSERT INTO tb_usuarios (deslogin,dessenha)	VALUES (pdeslogin,pdessenha);
-SELECT * FROM tb_usuarios WHERE idusuario = LAST_INSERT_ID();
-END
